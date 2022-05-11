63f1e3f47e8f30a33e077f20413cdcd5fd820e2b
tom twinkle <tomtinkle29@gmail.com>
2022-05-11 11:35:23 +0900

    Listview use card and slidable

--- lib/page3.dart
+++ lib/page3.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_provider/bloc_provider.dart';
 import 'package:board_buff_01/model/zenn_topic_article.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 import 'model/zenn_topic.dart';
 import 'page3_bloc.dart';
@@ -24,48 +25,88 @@ class _Page3State extends State<Page3> {
           itemCount: snapshot.data?.articles.length ?? 0,
           itemBuilder: (context, index) {
             ZennTopicArticle? article = snapshot.data?.articles[index];
-            return Text(article!.title);
+            String avatarImageUrl = article!.user.avatarSmallUrl;
+
+            Card cardWidget = Card(
+                color: Colors.white70,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween, // 右寄せ
+                  children: <Widget>[
+                    SizedBox(
+                      width: 80,
+                      height: 80,
+                      child: FadeInImage.assetNetwork(
+                        placeholder: 'assets/loading.gif',
+                        image: avatarImageUrl,
+                      ),
+                    ),
+                    ConstrainedBox(
+                      constraints:
+                          const BoxConstraints(minWidth: 200, maxWidth: 290),
+                      child: Text(
+                        article.title,
+                        textAlign: TextAlign.left,
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ],
+                ));
+
+            return Slidable(
+              key: UniqueKey(),
+              child: cardWidget,
+              endActionPane: ActionPane(
+                motion: const ScrollMotion(),
+                children: [
+                  SlidableAction(
+                    onPressed: (buildContext) {
+                      print("save!");
+                    },
+                    backgroundColor: Colors.red,
+                    foregroundColor: Colors.white,
+                    icon: Icons.save,
+                    label: 'Save',
+                  ),
+                ],
+              ),
+            );
           },
         );
       },
     );
 
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('ZennTopic検索'),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Topic:'),
-                  SizedBox(
-                    width: 150,
-                    child: TextFormField(
-                      keyboardType: TextInputType.number,
-                      onChanged: bloc.topicInputAction.add,
-                    ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ZennTopic検索'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Topic:'),
+                SizedBox(
+                  width: 200,
+                  child: TextFormField(
+                    keyboardType: TextInputType.text,
+                    onChanged: bloc.topicInputAction.add,
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      bloc.getTopic();
-                    },
-                    child: const Text('検索'),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 400,
-                // 配列を元にリスト表示
-                child: topics,
-              ),
-            ],
-          ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    bloc.getTopic();
+                  },
+                  child: const Text('検索'),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 600,
+              // 配列を元にリスト表示
+              child: topics,
+            ),
+          ],
         ),
       ),
     );
